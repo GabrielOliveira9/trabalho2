cc9e5973bb9e2e7dbe2a6abfc6edad7a4fda9862
Gabriel <gabrielmazzega2018@gmail.com>
2022-06-08 17:47:30 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+function save(){
+  alert("Seu cadastro foi concluído")
+}
+
+function myFunction() {
+  var click = document.getElementById("myLinks");
+  if (click.style.display === "block") {
+    click.style.display = "none";
+  } else {
+    click.style.display = "block";
+  }
+}
